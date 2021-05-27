94d1e04df2f95cf963bb6708b2f80533fd6398d9
rakus-NguyenSon <nhson@rakus.co.jp>
2021-05-27 11:54:50 +0900

    ホテルフォームクラスの作成

---
+++ src/main/java/com/example/demo/form/HotelForm.java
@@ -0,0 +1,20 @@
+package com.example.demo.form;
+
+/**
+ * ホテルを検索用のフォームクラス.
+ * 
+ * @author nhson
+ *
+ */
+public class HotelForm {
+	/** 値段*/
+	private String price;
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
